8e11d29d60387b44ba3d7456dc235a551ba1f20a
Masaaki Shibata <mshibata@emptypage.jp>
2020-08-16 16:01:00 +0900

    Read config JSON file.

--- discordbot.py
+++ discordbot.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python
+
+import json
+
+import discord
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+
+with open('discordbot-config.json') as f:
+    config = json.load(f)
+TOKEN = config['token']
+client.run(TOKEN)
